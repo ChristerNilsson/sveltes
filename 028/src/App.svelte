b92db379fe4f31cbf20f8b9fb24ecaafa44be5e5
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-11-29 12:18:26 +0100

    x

--- 028/src/App.svelte
+++ 028/src/App.svelte
@@ -1,55 +1,39 @@
 <script>
-	import P5Canvas from "./P5Canvas.svelte"
+	import P5 from "./P5.svelte"
 
-	let sketch = function(p5) {
-	  let w;
-	  let columns;
-	  let rows;
-	  let board;
-	  let next;
+	const sketch = function(p5) {
 
 	  p5.setup = () => {
-			p5.createCanvas(800, 800)
+			p5.createCanvas(400, 480)
 			p5.angleMode(p5.DEGREES)
+			p5.stroke(255,0,0)
+			p5.strokeWeight(50)
+			p5.noFill()
+		}
+
+		p5.one = () => {
+			p5.push()
+			p5.arc(-60,0,200,200,90,270)
+			p5.arc(60,0,200,200,-90,60)
+			p5.line(-50,-100,50,-100)
+			p5.line(-120,100,0,100)
+			p5.pop()
 		}
 
 	  p5.draw = () => {
 			p5.background(255)
-			p5.stroke(255,0,0)
-			p5.strokeWeight(50)
-			p5.noFill()
-			p5.translate(200,400)
 
-			// p5.rotate(-30)
+			p5.translate(200,180)
+			p5.rotate(-30)
+			p5.one()
 
-			p5.push()
-			p5.rotate(-90)
-			// p5.stroke(255,255,0)
-			p5.arc(0,0,200,200,180,330)
-			p5.rotate(90)
-			p5.translate(125,0)
-			p5.arc(0,0,200,200,-90,90)
-			// p5.stroke(0,255,0)
-			p5.line(-75,-100,60,-100)
-			p5.line(0,100,-125,100)
-			p5.pop()
-
-			p5.translate(60,-100)
-			p5.push()
-			p5.rotate(-90)
-			p5.arc(0,0,200,200,180,360)
-			p5.rotate(90)
-			p5.translate(125,0)
-			p5.arc(0,0,200,200,-90,60)
-			// p5.stroke(0,0,255)
-			p5.line(0,-100,-125,-100)
-			p5.line(-200,100,-50,100)
-			p5.pop()
-
-	  }
+			p5.translate(-60,100)
+			p5.rotate(180)
+			p5.one()
+		}
 	}
 </script>
 
 <main>
-  <P5Canvas sketch={sketch}/>
+	<P5 {sketch}/>
 </main>
---
+++ 028/src/P5.svelte
@@ -0,0 +1,7 @@
+<script>
+	import p5 from "./p5.min"
+	import { onMount } from "svelte"
+	export let sketch
+	onMount(function() {new p5(sketch)})
+</script>
+
